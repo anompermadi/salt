73cf53551d6c70a9cc47fd5ea5e2b664f579dbbb
Arsenius Anom Permadi <anom.permadi@outlook.com>
2023-07-30 17:35:50 +0700

    load profile image

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,6 +1,8 @@
 @extends('admin.admin_master')
 @section('admin')
 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
 <div class="page-content">
 <div class="container-fluid">
 
@@ -35,14 +37,14 @@
                 <div class="row mb-3">
                     <label for="example-text-input" class="col-sm-2 col-form-label">Profile Image</label>
                     <div class="col-sm-10">
-                        <input name="profile_image" class="form-control" type="file"  id="example-text-input">
+                        <input name="profile_image" class="form-control" type="file"  id="image">
                     </div>
                 </div>
                 <!-- end row -->
                 <div class="row mb-3">
                     <label for="example-text-input" class="col-sm-2 col-form-label"></label>
                     <div class="col-sm-10">
-                    <img class="rounded avatar-lg" src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
+                    <img class="rounded avatar-lg" id="showImage" src="{{ asset('backend/assets/images/small/img-5.jpg') }}" alt="Card image cap">
                     </div>
                 </div>
                 <!-- end row -->
@@ -55,5 +57,19 @@
 
 </div>
 </div>
+
+<script type="text/javascript">
+
+    $(document).ready(function(){
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    })
+
+</script>
 
 @endsection
